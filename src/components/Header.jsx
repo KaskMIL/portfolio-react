3d88af38bd8a0577fec77e23c32ba7dd7a164036
toMilanesi <tomi.milanesi@gmail.com>
2022-11-10 20:56:21 -0300

    Add state hook

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FaBars } from 'react-icons/fa';
 import MobileNav from './nav/mobile/MobileNav';
 
 const Header = () => {
+  const [sidebar, setSidebar] = useState(false);
   const title = 'This is a title';
+  const showSidebar = () => setSidebar(!sidebar);
+
   return (
     <header>
-      <FaBars />
-      <MobileNav>
-        <h2>{title}</h2>
-      </MobileNav>
+      <FaBars onClick={showSidebar} />
+      <MobileNav />
+      { title }
     </header>
   );
 };
